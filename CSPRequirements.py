4c75ce05f4b0edefb3847ee1c250de85439b9537
Hu9o73 <hbonnell@live.fr>
2024-11-18 00:37:42 +0100

    Working backtracking. All functions built so far are commented. Boats show up as only 'M' tiles, no differentiation of boat edges or submarine yet. First testable version. I might call it v0.1 for the sake of it.

--- CSPRequirements.py
+++ CSPRequirements.py
@@ -1,47 +1,36 @@
-# Variable, Constraints and CSP base classes to implement a problem as a CSP
+# Required imports:
+#   - Constraint functions
+#   - Parsing input (to read input files)
 from ConstraintDefinition import *
 from ParsingInput import *
-from itertools import product
-from baseChange import *
-import numpy as np
-
 
 class Variable:
-
+    '''Class used to define variables.'''
     def __init__(self, name, domain, state = None):
-        self.name = "VAR_" + name        # Name of our constraint
-        self.domain = list(domain)    # Values it can take
-        self.state = state
-        self.originalDomain = list(domain)  # Keeping a copy of the originalDomain for later backtracking
+        self.name = "VAR_" + name           # Name of our constraint
+        self.domain = list(domain)          # Values it can take
+        self.state = state                  # Base state is None
 
     def __str__(self):
         return "Variable {} | State : {} | Domain : {}\n".format(self.name, self.state, self.domain)
     
 
     def setState(self, state):
-        if state in self.domain or state == "0" or state == None:
-            self.state = state
+        '''Function to set the state "properly". Verifying that the state passed as a parameter is in the domain.'''
+        
+        if state in self.domain or state == "0" or state == None:   # Check if state is in the domain, or 0, or None (for u)
+            self.state = state                                      # Assign domain
             pass
         else:
             pass # Do nothing...
-
-
-    def remove_from_domain(self, value):
-        if value in self.domain:
-            self.domain.remove(value)
-
-    def restore_domain(self):
-        """ Restore the domain to its original state during backtracking. """
-        self.domain = list(self.originalDomain)
-
-    
     
 
 
 class Constraint:
+    '''Class used t define constraints. Scope are variables related to this constraint.'''
 
     def __init__(self, name, scope, function = lambda: True):
-        self.name = "CONS_" + name            # Name of our constraint
+        self.name = "CONS_" + name  # Name of our constraint
         self.scope = list(scope)    # Scope of the constraint => Variables related to our constraint
         self.function = function    # Assigning the bool function that states if our constraint is respected or not. If nothing specified, we return True no matter what
 
@@ -50,6 +39,9 @@ class Constraint:
 
     
     def check(self):
+        '''Check if constraint is respected by applying its associted function to its scope
+        (= the variables related to the constraint.)'''
+
         if self.function(self.scope) == False:
                 return False
         
@@ -57,11 +49,12 @@ class Constraint:
     
 
 class CSP:
+    '''Base class for Constraint Satisfaction Problem declaration.'''
 
     def __init__(self, name, variables, constraints):
-        self.name = "CSP_" + name
-        self.variables = variables
-        self.constraints = constraints
+        self.name = "CSP_" + name       # Name of the problem
+        self.variables = variables      # Variables of the problem
+        self.constraints = constraints  # Constraints of the problem. Each constraint has variables related to it. Make sure they're included inthe variables list.
 
     def __str__(self):
         return self.name
@@ -69,161 +62,181 @@ class CSP:
 
 
 def solutionGridBuilder(solution, rows=6, cols=6):
-    matrix = [["" for _ in range(cols)] for _ in range(rows)]
+    '''Returns a matrix representing the grid of a solution. Solution is inputed as a dictionnary.'''
+
+    matrix = [["" for _ in range(cols)] for _ in range(rows)]   # Create an "empty" row*col matrix.
 
     # Fill the matrix
-    for var, value in solution.items():
-        # Extract coordinates from variable name
-        __, ___, row, col = var.name.split("_")
-        row, col = int(row), int(col)
-        matrix[row][col] = value
+    for var, value in solution.items():             # For all items in the solution dictionary (var, corresponding value)
+        __, ___, row, col = var.name.split("_")     # Extract coordinates from variable name. Split on "_". Variables formated as VAR_NAME_ROW_COL
+        row, col = int(row), int(col)               # Turn the row and col values to integers
+        matrix[row][col] = value                    # Assign to the row/col coordinates the vaue.
 
-    return matrix
+    return matrix                                   # Return the matrix
 
 
 
 
 def defineLineConstraints(bspb, gridVar, direction, label="Line"):
-    lineCount = 0
-    for line in gridVar:
-        array = []
-        array.append(direction[lineCount])
+    '''
+    Function to define a "linear constraint". Direction being horizontal or vertical.
+    We build an array with the target value and a line's variables.
+    GridVar is a matrix [[],[],[]]. Each array in the matrix is either a row or a column.
+    We concatenate the target and the values and create a constraint on this array.
+    The associated function is "isLineRespected". See its definition in ConstraintDefinition.py
+    '''
 
-        for value in line:
-            array.append(value)
+    lineCount = 0                               # Count how many lines we dealt with yet
+    for line in gridVar:                        # For every line in GridVar (= for every row or every col)
+        array = []                              # Initialize an array
+        array.append(direction[lineCount])      # We add the target value. Direction is an array where each value with id=i is the target variable of line n°i
 
-        cons = Constraint("{}_{}".format(label, lineCount), array, isLineRespected)
-        bspb.constraints.append(cons)
-        lineCount +=1
+        for value in line:                      # For every value in the line
+            array.append(value)                 # We add this value to our array, thus after the target value
+
+        cons = Constraint("{}_{}".format(label, lineCount), array, isLineRespected)   # We build a constraint based on this array, with function isLineRespected.
+        bspb.constraints.append(cons)           # We add the constraint to the battleship CSP problem given as parameter
+        lineCount +=1                           # We add one line to the final count
 
 
 
 
 class BattleShipProblem(CSP):
+    '''
+    Class for Battleship problem definiton and solving.
+    Based on the CSP class.
+    '''
 
     def __init__(self, name, filepath):
-        super().__init__(name=name, variables=[], constraints=[])
         
-        horizontal, vertical, ships, grid = parse_battleship_input(filepath)
+        super().__init__(name=name, variables=[], constraints=[])               # Initialize the CSP class
+        
+        horizontal, vertical, ships, grid = parse_battleship_input(filepath)    # Get the grid, ships, target line values from the filepath, using the parse input function
 
-        self.grid = grid
-        self.gridVarRow = []
-        self.gridVarCol = []
+        self.grid = grid                                                        # Defining the "grid" as an array
+        self.gridVarRow = []                                                    # Initialize the gridVarRow, a matrix where each element is an array representing a row
+        self.gridVarCol = []                                                    # Initialize the gridVarRow, a matrix where each element is an array representing a column
         #self.domain = ['0','.','S','<','>','M','^','v']
-        self.domain = ['.','M']
+        self.domain = ['.','M']                                                 # Defining the domain. We only use M and . for efficiency purposes. Considering M tiles as ships, depending on where they are we can know what exact type it is (^,v,>,<,S)
         
+        self.horizontal = horizontal                # Used to return these values if needed
+        self.vertical = vertical                    # Used to return these values if needed
 
         # Defining gridVarRow and gridVarCol
         rowNum = 0
         colNum = 0       
 
-        for row in grid:
-            tempRow = []
-            for column in row:
-                var = Variable("Cell_{}_{}".format(rowNum, colNum), self.domain, column)
-                tempRow.append(var)
-                self.variables.append(var)
+        for row in grid:                            # For every row in the grid
+            tempRow = []                            # Create a temporary row
+            for column in row:                      # For every value in this row, thus every column
+                var = Variable("Cell_{}_{}".format(rowNum, colNum), self.domain, column)    # We create a variable corresponding to that cell
+                tempRow.append(var)                 # Add this variable to our temporary row
+                self.variables.append(var)          # We add the variable to the variables array of our CSP problem
 
-                colNum +=1
+                colNum +=1                          # Column counter used to name variables
             
-            self.gridVarRow.append(tempRow)
-            colNum = 0
-            rowNum +=1
-        
-        for i in range(len(self.gridVarRow[0])):
-            tempCol = []
-            for j in range(len(self.gridVarRow)):
-                tempCol.append(self.gridVarRow[j][i])
+            self.gridVarRow.append(tempRow)         # We add the temporary row as an actual row in "gridVarRow"
+            colNum = 0                              # Reset column counter (coming back to col 0)
+            rowNum +=1                              # Passing to the next line, used to name the variables
 
-            self.gridVarCol.append(tempCol)
+        for i in range(len(self.gridVarRow[0])):        # For every column
+            tempCol = []                                # We create a temp column
+            for j in range(len(self.gridVarRow)):       # For every row in that column
+                tempCol.append(self.gridVarRow[j][i])   # We add to our temp column its value (the variable)
+
+            self.gridVarCol.append(tempCol)             # Once done, we add the full temp Column to the gridVarCol matrix
 
 
         # Setting the constraints
-        defineLineConstraints(self, self.gridVarRow, horizontal, "ROW")
-        defineLineConstraints(self, self.gridVarCol, vertical, "COL")
+        defineLineConstraints(self, self.gridVarRow, horizontal, "ROW")     # Using defineLine function to set the row constraints
+        defineLineConstraints(self, self.gridVarCol, vertical, "COL")       # Using defineLine function to set the col constraints
     
 
-    def backtracking_search(self, assignment = {}):
-        return self.recursive_backtracking(assignment)
+    def solve(self, method):
+        '''
+        To call different solvers.\n
+        Available solvers :\n
+            "backtracking"\n
+        Returns solution (dictionary)
+        '''
+        if(method == "backtracking"):
+            return self.backtracking_search()
+        else:
+            return None
         
 
-
+    def backtracking_search(self, assignment = {}):
+        '''
+        Perform a backtracking search to solve the problem.
+        If it finds a solution, its yielded as a dictionnary, where each variable of our problem has an associated state.
+        '''
+        return self.recursive_backtracking(assignment)  # Start backtracking recursion
+        
 
     def recursive_backtracking(self, assignment):
-        self.printGrid()
-        if self.is_complete(assignment):
-            print("Assignment is complete !")
-            return assignment
+        '''Recursive backtracking function.'''
 
-        print("Selecting variable...")
-        var = self.select_unassigned_variable(assignment)
-        print("Variable selected : ", var)
+        if self.is_complete(assignment):                    # If assignment is complete (all variables assignated + constraints validated)
+            return assignment                               # We return the solution 
 
-        if not var:
-            print("Variable not valid")
-            return None  # No unassigned variable found
+        var = self.select_unassigned_variable(assignment)   # (Else) We select and unassigned variable
 
-        print("Starting loop...")
-        for value in var.domain:
-            print("Value selected : ", value)
-            if self.is_consistent(var, value):
-                print(f"BSP consistent. Trying {value} for {var.name}")  # Debugging
-                var.setState(value)
-                print("State set: ", var.state)
-                assignment[var] = value
-                print("Assignment set: ", assignment[var])
+        if not var:                                         # If we don't get a variable
+            return None                                     # We return None, as no new variable is available
 
-                result = self.recursive_backtracking(assignment)
-                if result is not None:
-                    print("Returning a not None result: ", result)
+        for value in var.domain:                            # Otherwise, for every value in the variable's domain ( [., M] normally)
+            if self.is_consistent(var, value):              # We check the consistency of our solution if we temporarily set the variable's state to the given value
+                var.setState(value)                         # If it is, we set the variable's state to the given value
+                assignment[var] = value                     # And add this change to our final solution
+
+                result = self.recursive_backtracking(assignment)    # Recursion, to go on to the next variable
+
+                if result is not None:                      # If the result is not None, means that a solution was found so we return it
                     return result
 
-                # Backtrack
-                print(f"Backtracking on {var.name}")
-                del assignment[var]
-                var.setState("0")
-                print("Backtracking... updated state: ", var.state)
-                self.printGrid()
+                # Backtrack                                  If we ever come back to this place, it means that result was None, meaning that no solution was found
+                del assignment[var]                         # We delete the assignment in our solution
+                var.setState(None)                          # And set the state back to None (or 0, which would mean unassigned)
 
-        print("Returning none...")
-        return None
+        return None                                         # After having check all the possible value, if no solution was found we return None
 
     
-    
-
     def printGrid(self):
-        for row in self.gridVarRow:
-            for value in row:
-                print(value.state, end=" ")
-            print()
+        '''Printing the grid of the batleship problem. Prints the states of each variable in a human-readable grid format.'''
 
-        print("-----------")
+        for row in self.gridVarRow:             # For each row
+            for value in row:                   # We check every value in the row
+                print(value.state, end=" ")     # Print them on the same line
+            print()                             # Print new line for every row
+        print("-----------")                    # Print separator
 
-
-    def is_complete(self, assignment):
-        # Check if all variables are assigned and all constraints are satisfied
-        return len(assignment) == len(self.variables) and all(cons.check() for cons in self.constraints)
-    
 
     def select_unassigned_variable(self, assignment):
-        # First unassigned variable is chosen
-        for var in self.variables:
-            if var not in assignment:
-                return var
+        '''Selects an unassigned variable, passing the assignment as a parameter to know what variables are assigned.
+            Simply returns the next unassigned variable.'''
+        
+        for var in self.variables:          # For all variables
+            if var not in assignment:       # If var is not assigned
+                return var                  # We return it
+            
+        return None                         # Otherwise we return None
 
-        return None
-    
+
     def is_consistent(self, var, value):
-        var.setState(value)
-        #print("TempVar : ", var)
-        for cons in self.constraints:
-            if var in cons.scope:
-                print(cons)
-
-            if var in cons.scope and not cons.check():
-                var.setState(None)
-                return False
-        var.setState(None)
-        return True
+        '''For the given variable, checks if setting its state to the value inputed as a parameter
+            breaks the constraints the variable is related to.'''
+        
+        var.setState(value)                                 # Setting the state temporarily
+        for cons in self.constraints:                       # For all constraints in the bs problem
+            if var in cons.scope and not cons.check():      # If the variable is in its scope and breaks the constraints
+                var.setState(None)                          # We set the state to None
+                return False                                # And return false
+        var.setState(None)                                  # Otherwise we reset the state
+        return True                                         # But return true
+    
+    def is_complete(self, assignment):
+        '''Check if all variables are assigned and all constraints are satisfied'''
+        return len(assignment) == len(self.variables) and all(cons.check() for cons in self.constraints)
+        
     
     
--- ConstraintDefinition.py
+++ ConstraintDefinition.py
@@ -1,26 +1,30 @@
 def isLineRespected(array):
     '''
+    Used to check if a line's target value is respected.
     The parameter should an array with:
     [0] = targetValue
     [:] = the row you're checking (i.e. [.,.,<,M,>,.])
     The function will then take the first value of the array you passed as parameter to check if the
     number of "ship tile" corresponds to the value in array[0]. 
     '''
-    target = array[0]
-    count = 0
-    unassigned = 0
+    target = array[0]                                               # We get the target from the first value of our array
+    count = 0                                                       # To count the "boats" in my line
+    unassigned = 0                                                  # To count the unassigned values
 
-    for var in array[1:]:
-        #print(var.state, end="")
-        if var.state in ['S', 'M', '<', '>', '^', 'v']:  # Valid ship parts
-            count += 1
-        elif var.state == None or var.state == '0':  # Unassigned cell
-            unassigned += 1
-    
-    #print()
-    #for val in array[1:]:
-        #print("Name : ", val.name, " | state : ", val.state, end= " | ")
-    
-    #print("Count:", count, " | unassigned: ", unassigned, " | Target : ", target)
-    # Constraint is satisfied if we can still potentially reach the target
+    for var in array[1:]:                                           # For every variable in the array, from index 1 to the end
+        if var.state in ['S', 'M', '<', '>', '^', 'v']:             # If the state is a valid ship parts
+            count += 1                                              # We count 1 ship
+        elif var.state == None or var.state == '0':                 # Else if the state is None or 0
+            unassigned += 1                                         # We count an unassigned variable
+
+    # We want to check that the boat count == the target
+    # But if we simply put count == target, having 1 boat out of 3 (for example), will state that the condition isn't respected
+    # And thus, that the solution isn't consistent forcing us to try another variable (because of how the script is made)
+    #
+    # To check consistency:
+    # We return True if the count <= target and the count + unassigned values >= target
+    # This way, the function will yield True if count == target (because unassigned would be 0) or if we still can place ships
     return count <= target and (count + unassigned) >= target
+
+
+# Note : Completeness will be checked in an "is_complete" function, making sure the solution is consitent AND all variables are assigned.
